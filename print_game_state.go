10558b99801633a6dde30a39589301c17c20da32
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-25 02:34:06 +0000

    extraction process complete

--- print_game_state.go
+++ print_game_state.go
@@ -3,32 +3,51 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil"
+	"strconv"
 	dem "github.com/markus-wa/demoinfocs-golang"
 	//ex "github.com/markus-wa/demoinfocs-golang/examples"
 	events "github.com/markus-wa/demoinfocs-golang/events"
-	"strings"
 )
-
-func processDemoFile(demPath string){
+// exists returns whether the given file or directory exists
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil { return true, nil }
+	if os.IsNotExist(err) { return false, nil }
+        return true, err
+}
+func processDemoFile(demPath string,file_id int){
 	f, err := os.Open(demPath)
-	defer f.Close()
 	checkError(err)
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Erro no processamento do arquivo!", r)
+		}
+	}()
 	p := dem.NewParser(f)
-
-	// Parse header
+	defer f.Close()
 	header, err := p.ParseHeader()
 	checkError(err)
 	fmt.Println("Map:", header.MapName)
-	
+	dir_name:="/home/jupyter/mrdbarros/csgo_analyze/data/"+header.MapName
+	dir_exists,_:=exists(dir_name)
+	if !dir_exists{
+		err=os.Mkdir(dir_name,0700)
+		checkError(err)
+	}
+	new_file:=dir_name+"/"+header.MapName+"_"+ strconv.Itoa(file_id)+".txt"
+	file_write, err := os.Create(new_file)
+	checkError(err)
+	current_state:=""
+
+	defer file_write.Close()
 	
 	p.RegisterEventHandler(func(e events.Kill) {
 		killer_team := e.Killer.Team
 		if killer_team == 2 {
-			fmt.Printf("t_kill ")
+			current_state+="t_kill "
 		} else if killer_team == 3 {
-			fmt.Printf("ct_kill ")
+			current_state+="ct_kill "
 		}
 	})
 	
@@ -36,66 +55,51 @@ func processDemoFile(demPath string){
 		
 		win_team := e.Winner
 		if win_team == 2 {
-			fmt.Printf("t_round_win ")
+			current_state+="t_round_win "
 		} else if win_team == 3 {
-			fmt.Printf("ct_round_win ")
+			current_state+="ct_round_win "
 		} else {
-			fmt.Printf("invalid_round_end ")
+			current_state+="invalid_round_end "
 		}
 	})
 	
 	p.RegisterEventHandler(func(e events.RoundStart) {
-		
-
-		fmt.Printf("round_start ")
+		current_state+="round_start "
 	})
 	
 	p.RegisterEventHandler(func(e events.RoundEndOfficial) {
-		
 
-		fmt.Printf("round_end_official ")
+		current_state+="round_end_official "
 	})
-	
 	p.RegisterEventHandler(func(e events.BombPlanted) {
-		
 
-		fmt.Printf("bomb_planted ")
+		current_state+="bomb_planted "
 	})
-	
 	p.RegisterEventHandler(func(e events.BombPlantBegin) {
-		
 
-		fmt.Printf("bomb_plant_begin ")
+		current_state+="bomb_plant_begin "
 	})
-	
-	
-	
 	err = p.ParseToEnd()
 	checkError(err)
 
-
+	_, err = file_write.WriteString(current_state)
+	checkError(err)
 	// Parse to end
 }
 
 // Run like this: go run print_events.go -demo /path/to/demo.dem
 func main() {
-	demSetPath:="C:/Users/marcel.barros/go/src/github.com/markus-wa/demoinfocs-golang/data"
-
-	dems, err := ioutil.ReadDir(demSetPath)
+	dem_path:= os.Args[1]
+	file_id_str:=os.Args[2]
+	file_id, err := strconv.Atoi(file_id_str)
 	checkError(err)
-	for _, d := range dems {
-		name := d.Name()
-		if strings.HasSuffix(name, ".dem") {
-			fmt.Printf("Parsing '%s/%s'\n", demSetPath, name)
-			processDemoFile(demSetPath+"/"+name)
-	
-		}
-	}
+	processDemoFile(dem_path,file_id)
 }
 
 
 func checkError(err error) {
 	if err != nil {
+		print("error!")
 		panic(err)
 	}
 }
